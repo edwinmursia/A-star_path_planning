64f4f332234a3760057e933b3054d06e813d9ce4
edwinmursia <edwin.mursia@edu.turkuamk.fi>
2021-05-12 19:43:38 +0300

    Made the window visible with its attributes.

--- a_star.py
+++ a_star.py
@@ -1,8 +1,9 @@
+from astar import Spot
 import pygame
 import math
 from queue import PriorityQueue
 
-from pygame.constants import GL_GREEN_SIZE
+from pygame.constants import GL_GREEN_SIZE, QUIT
 
 # Setting up the window.
 WIDTH = 800
@@ -54,6 +55,9 @@ class Node:
     def reset(self):
         self.color == WHITE
 
+    def make_start(self):
+        self.color = ORANGE
+
     def make_closed(self):
         self.color == RED
 
@@ -84,3 +88,84 @@ def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+# Builds the grid.
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            spot = Node(i, j, gap, rows)
+            grid[i].append(spot)
+
+    return grid
+
+# Gridline drawer.
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+        for j in range(rows):
+            pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+
+# Draws everything.
+def drawer(win, grid, rows, width):
+    win.fill(WHITE)
+
+    for row in grid:
+        for spot in row:
+            spot.draw(win)
+
+    draw_grid(win, rows, width)
+    pygame.display.update()
+
+# Recognizes the square that is clicked.
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    x, y = pos
+
+    row = y // gap
+    col = x // gap
+    return row, col
+
+def main(win, width):
+    ROWS = 50
+    grid = make_grid(ROWS, width)
+
+    start = None
+    end = None
+
+    run = True
+    started = False
+    while run:
+        drawer(win, grid, ROWS, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if started:
+                continue
+
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                if not start:
+                    start = spot
+                    start.make_start()
+
+                elif not end:
+                    end = spot
+                    end.make_end()
+
+                elif spot != end and spot != start:
+                    spot.make_barrier()
+
+            elif pygame.mouse.get_pressed()[2]:
+                pass
+
+            
+    pygame.quit()
+
+main(WIN, WIDTH)
